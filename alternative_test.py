d2117d8fb183b8b94574fb884851806f5b7c79b8
martinalon <martinalonsofloresgonzalez@gmail.com>
2023-05-18 17:33:39 -0600

    adding comments to the .py files

--- alternative_test.py
+++ alternative_test.py
@@ -10,7 +10,7 @@ import random as rn
 from image_processing import obtener_extension_archivos, construir_base_datos, train_validation_test_sets
 from tensorflow import keras
 
-
+### Loading the data bases from alternative folders, these are diferents of the original ones
 Imagenes=[]
 Etiquetas=[]
 IMG_TAM = 150
@@ -18,15 +18,16 @@ DIR_BASE =os.path.abspath(os.path.dirname(__file__))
 DIR_no_relation = DIR_BASE+'/scatter_plots/alternative_test/no_relation'
 DIR_relation = DIR_BASE+'/scatter_plots/alternative_test/relation'
 
-
+# Creating only one database with all the images preprocessed
 Imagenes, Etiquetas = construir_base_datos([], [], 0, DIR_no_relation, "grayscale", IMG_TAM)
 Imagenes, Etiquetas = construir_base_datos(Imagenes, Etiquetas, 1, DIR_relation, "grayscale", IMG_TAM)
 
+# making the bases into np arrays
 Imagenes = np.array(Imagenes)
 Etiquetas = np.array(Etiquetas)
 
-
-my_model = tf.keras.models.load_model('modelo_hecho_desde_cero.h5')
+#importing the model again to make  the test.
+my_model = tf.keras.models.load_model('model1.h5')
 tasa_aprendizaje_base = 0.0000005
 my_model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=tasa_aprendizaje_base), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
@@ -35,3 +36,6 @@ perdida_prueba_pre_entrenado, exactitud_prueba_pre_entrenado =  my_model.evaluat
                                                                                       y = Etiquetas,
                                                                                       batch_size=200,
                                                                                       verbose=1)
+
+print(perdida_prueba_pre_entrenado)
+print( exactitud_prueba_pre_entrenado)
--- classifier.py
+++ classifier.py
@@ -10,7 +10,7 @@ import random as rn
 from image_processing import obtener_extension_archivos, construir_base_datos, train_validation_test_sets
 from tensorflow import keras
 
-
+### Loading the data bases from folders
 Imagenes=[]
 Etiquetas=[]
 IMG_TAM = 150
@@ -19,19 +19,15 @@ DIR_no_relation = DIR_BASE+'/scatter_plots/no_relation'
 DIR_relation = DIR_BASE+'/scatter_plots/relation'
 
 
+# Creating only one database with all the images preprocessed
 Imagenes, Etiquetas = construir_base_datos([], [], 0, DIR_no_relation, "grayscale", IMG_TAM)
 Imagenes, Etiquetas = construir_base_datos(Imagenes, Etiquetas, 1, DIR_relation, "grayscale", IMG_TAM)
 clases=["No Relation", "Relation"]
 
-
+# Creating the sets for train , validation and test
 img_train, etq_train, img_val, etq_val, img_test, etq_test = train_validation_test_sets(Imagenes, Etiquetas, 0.05, 0.05)
 
-print(len(etq_train))
-print(len(etq_val))
-print(len(etq_test))
-
-
-#
+# Making a generator of new images by flipping the original ones
 datagen = tf.keras.preprocessing.image.ImageDataGenerator(
             rotation_range=0,  # rotación aleatoria de la imágenes en un rango de (0 a 30)
             zoom_range = 0.05, # acercamiento aleatorio dentro de la imagen
@@ -42,7 +38,7 @@ datagen = tf.keras.preprocessing.image.ImageDataGenerator(
             channel_shift_range=0.5   #modificación aleatorio de los valores RGB de la imagen
             )
 
-
+# Defining the architecture of the convolutional neural network
 redConv1 = tf.keras.Sequential([
   tf.keras.layers.Conv2D(6, (5,5),activation='relu',padding='Same', input_shape = (150,150,1)),  
   tf.keras.layers.MaxPooling2D((2,2)),  
@@ -58,13 +54,16 @@ redConv1 = tf.keras.Sequential([
   tf.keras.layers.Dense(50, activation="relu"),
   tf.keras.layers.Dense(2,activation='relu')
   ])
-print(redConv1.summary())
 
+#print(redConv1.summary())
+
+# Defining the parameters for the training and starting it 
 opt = keras.optimizers.Adam(learning_rate=0.0000005)
 redConv1.compile(optimizer=opt,loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 evolucion = redConv1.fit(datagen.flow(img_train, etq_train, batch_size=200),validation_data=(img_val, etq_val,), epochs=400, batch_size=200)
 
 
+# ploting the ovolution of the loss and accuracy functions
 plt.figure(figsize=(10, 10))
 plt.subplot(1, 2, 1)
 plt.plot(evolucion.history['loss'])
@@ -82,9 +81,10 @@ plt.ylabel('Exactitud')
 plt.xlabel('Epocas')
 plt.legend(['entrenamiento', 'prueba'])
 
-
+#saving the neural network
 redConv1.save("model2.h5")
 
+#importing the model again to realice  the test. see also the alternative_test.py
 my_model = tf.keras.models.load_model('model2.h5')
 tasa_aprendizaje_base = 0.0000005
 my_model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=tasa_aprendizaje_base), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
@@ -94,7 +94,7 @@ perdida_prueba_pre_entrenado, exactitud_prueba_pre_entrenado =  my_model.evaluat
                                                                                       y = etq_prueba,
                                                                                       batch_size=200,
                                                                                       verbose=1)
-
+#printing the results
 print(perdida_prueba_pre_entrenado, exactitud_prueba_pre_entrenado)
 
 
